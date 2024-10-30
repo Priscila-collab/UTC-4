75d3dd61f6b3a1ac8044a5376f103448401f04fe
Priscila-collab <priscila.ruiz3880@utc.edu.ec>
2024-10-30 10:27:24 -0500

    Update funcion.py

--- funcion.py
+++ funcion.py
@@ -1,4 +1,15 @@
-def sumar(n1,n2):
-    sumar=n1+n2
-    return sumar
-print("la suma es:",sumar(5,6))
+def sumar (n1,n2): #funcion con susparametros para asignar valores a sus variables 
+    return n1+n2 
+def restar (n1,n2):
+    return n1-n2 
+def multiplicacion (n1,n2):
+    return n1*n2
+def divicion (n1,n2):
+    return n1/n2
+n1=int (input("ingresa el numero 1: ")) # almacenar los valores
+n2=int (input("ingresa el numero 2: "))
+
+print ("la suma es :", sumar (n1,n2))
+print ("la resta es :", restar (n1,n2))
+print ("la multiplicacion es :", multiplicacion (n1,n2))
+print ("la division es :", divicion (n1,n2))
