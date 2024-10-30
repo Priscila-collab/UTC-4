46a48757d7fd01efab7a922cc9f10d5c27f4e037
Priscila-collab <priscila.ruiz3880@utc.edu.ec>
2024-10-30 10:27:15 -0500

    Create raiz.py

---
+++ raiz.py
@@ -0,0 +1,11 @@
+# obtener la raiz cuadrada de un numero
+import math
+
+def calcular_raiz_cuadrada(num):
+  return math.sqrt(num)
+
+
+num = int(input("Ingresa el numero: "))
+print("La raiz de" , num , " es:", calcular_raiz_cuadrada(num))
+print(f"La raiz de {num} es: {calcular_raiz_cuadrada(num)}")
+print("La raiz de {num} es: {calcular_raiz_cuadrada(numero)}")
